d8c9bdedb7af48add5db272be154860279fc471a
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 12:34:58 +0530

    Create PID Control .py

---
+++ PID Control .py
@@ -0,0 +1,13 @@
+# SESSION 40 – PID Control (Proportional)
+
+# Step 1: Input values
+error = 12
+Kp = 0.12
+
+# Step 2: Compute control output
+u = Kp * error
+
+# Step 3: Display result
+print("Control Output =", u)
+
+print("\nProgram Executed Successfully")
